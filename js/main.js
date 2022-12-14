22e7825ab4f76c5e21b8c13ddeeb64b3ac8d23e0
nhatnv6767 <nhatnvse90183@gmail.com>
2022-12-15 00:13:15 +0900

    weather 3.1

--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
 const main = () => {
     const address = document.getElementById("txtAddress").value
+    // const callback
     // dung superagent call api cua google, lay toa do cua dia chi nguoi dung
     superagent.get(`https://maps.googleapis.com/maps/api/geocode/json?key=AIzaSyDBunJ4GXNEC3KJlpoGJO-iB--CjPv4o-s&address=${address}`)
-    
+        .end((err, res) => {
+            console.log(res)
+        });
+
 }
