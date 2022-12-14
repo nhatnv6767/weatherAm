a16edfe1cddfc19a6ba645c66e426d2631789eea
nhatnv6767 <nhatnvse90183@gmail.com>
2022-12-15 00:26:14 +0900

    weather 4 temperate

--- js/main.js
+++ js/main.js
@@ -3,8 +3,25 @@ const main = () => {
     // dung superagent call api cua google, lay toa do cua dia chi nguoi dung
     superagent.get(`https://maps.googleapis.com/maps/api/geocode/json?key=AIzaSyDBunJ4GXNEC3KJlpoGJO-iB--CjPv4o-s&address=${address}`)
         .end((err, res) => {
+            if (err) {
+                console.log(err)
+                return
+            }
             const {lat, lng} = res.body.results[0].geometry.location
-            console.log(lat, lng)
+
+            //dùng superagent call api của darksky, lấy thời tiết của địa chỉ người dùng nhập
+            superagent
+                .get(
+                    `https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/7bbecca28cbc31d7c6739e70baa64e46/${lat},${lng}`
+                )
+                .end((err, res) => {
+                    if (err) {
+                        console.log(err)
+                        return;
+                    }
+                    console.log(res)
+
+
         });
 
 }
