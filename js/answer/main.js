27939df9f0d8e6a19f1ccd3d2c438eaa712e37a0
nhatnv6767 <nhatnvse90183@gmail.com>
2022-12-18 22:17:37 +0900

    Answer the question 4.4

--- js/answer/main.js
+++ js/answer/main.js
@@ -1,13 +1,24 @@
+let questionList = []
 const fetchQuestion = async () => {
     try {
         const res = await axios({
             url: "https://5bd2959ac8f9e400130cb7e9.mockapi.io/api/questions",
             method: "GET",
         })
-        console.log(res)
+
+        questionList = res.data
     } catch (e) {
         console.log(e)
     }
+}
+
+const renderQuestion = () => {
+    let htmlContent = "";
+    for (let item of questionList) {
+        htmlContent += item.render()
+    }
+
+    document.getElementById("questionsContainer").innerText = htmlContent;
 }
 
 fetchQuestion()
