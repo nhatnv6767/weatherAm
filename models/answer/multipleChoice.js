c9565107ba21bbd3421dac71f29cab42d4aa483a
nhatnv6767 <nhatnvse90183@gmail.com>
2022-12-18 21:47:24 +0900

    Answer the question 3.3

--- models/answer/multipleChoice.js
+++ models/answer/multipleChoice.js
@@ -2,4 +2,14 @@ class MultipleChoice extends Question {
     constructor(type, id, content, answers) {
         super(type, id, content, answers);
     }
+
+    render() {
+        return `
+            <div>
+                <p>
+                    Some kind of question
+                </p>
+            </div>
+        `
+    }
 }
